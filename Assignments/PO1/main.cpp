f5da950e6b133010baefb5b0dce3a8cfeebe99e7
Fatima Marquez <123118716+FatimaMarq14@users.noreply.github.com>
2023-02-07 10:10:05 -0600

    Update main.cpp

--- Assignments/PO1/main.cpp
+++ Assignments/PO1/main.cpp
@@ -1,1 +1,17 @@
-
+/*****************************************************************************
+*                    
+*  Author:           (Fatima Marquez)
+*  Email:            (fatimamarquez0914@gmail.com)
+*  Label:            (PO1)
+*  Title:            (Vector Class)
+*  Course:           (CMPS 2143)
+*  Semester:         (Spring 2022)
+* 
+*  Description:
+*        Include my own version of a Vector Class
+* 
+*  Usage:
+*        
+* 
+*  Files:            (main.cpp)
+*****************************************************************************/
